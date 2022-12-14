c4de8b106fa281994976da5467b9ecb614d3bc4a
Thibault Bruggeman <thibault.bruggeman@zaclys.net>
2022-12-14 19:45:06 +0100

    Init Spring project

---
+++ src/main/java/com/thibaultbman/myrecipesmanager/MyRecipesManagerApplication.java
@@ -0,0 +1,13 @@
+package com.thibaultbman.myrecipesmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyRecipesManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyRecipesManagerApplication.class, args);
+	}
+
+}
